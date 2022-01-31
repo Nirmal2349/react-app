59a695003ad90d86a56f946332579052d4fcefd0
Nirmal2349 <nirmalrajr35@gmail.com>
2022-01-31 09:59:53 +0530

    movie1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import { useState } from "react";
+
 import "./App.css";
 
 export default function App() {
   // const names = ["nemo", "rock", "jack"];
   const users = [
+    // {
+    //   name: "nemo",
+    //   image:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpXxXgL-IJd-HkjQKIU6RcawDSm8YO11f0Ww&usqp=CAU"
+    // },
+    // {
+    //   name: "jack",
+    //   image:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3C1TqfEFZAIzWESqgbMD8ud96aPz1PB6_hQ&usqp=CAU"
+    // },
+    // {
+    //   name: "rock",
+    //   image:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5svSIP5sxJ55Pav_GYjdd1LJo9IWHdnErLw&usqp=CAU"
+    // }
     {
-      name: "nemo",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpXxXgL-IJd-HkjQKIU6RcawDSm8YO11f0Ww&usqp=CAU"
-    },
-    {
-      name: "jack",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3C1TqfEFZAIzWESqgbMD8ud96aPz1PB6_hQ&usqp=CAU"
-    },
-    {
-      name: "rock",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5svSIP5sxJ55Pav_GYjdd1LJo9IWHdnErLw&usqp=CAU"
+      name:"Black clover",
+      image:"https://data.whicdn.com/images/342427514/original.gif"
+    },{
+      name:"One punch man",
+      image:"https://i.pinimg.com/originals/8e/11/36/8e1136c1c39d90548e30bfb75db113e8.gif"
+    },{
+      name:"Haikyuu",
+      image:"https://c.tenor.com/1l1pf5sZLWUAAAAC/haikyuu-jump.gif"
+    },{
+      name:"Demon slayer",
+      image:"https://www.gifcen.com/wp-content/uploads/2021/02/demon-slayer-gif-10.gif"
+    },{
+      name:"Naruto",
+      image:"https://i.pinimg.com/originals/6b/4c/53/6b4c53c16384d6e7fb8e8526a13a0913.gif"
     }
   ];
 
@@ -39,6 +57,7 @@ export default function App() {
       {users.map((usr) => (
         <Hai name={usr.name} image={usr.image} />
       ))}
+      <Counter/>
     </div>
   );
 }
@@ -55,8 +74,21 @@ function Hai({ name, image }) {
   return (
     <div className="new">
       <img src={image} alt="not supported" />
-      <h1>haiiii {name}👻👻🤖</h1>
+      <h1>{name}🎬</h1>
+      
     </div>
   );
+}
+
+function Counter(){
+  const[like,setLike]=useState(0)
+  const[dislike,setdisLike]=useState(0)
+  return(
+    <div>
+      <button onClick={()=>setLike(like+1)}>👍{like}</button>
+      <button onClick={()=>setdisLike(dislike+1)}>👎{dislike}</button>
+      
+      </div>
+  )
 }
 
