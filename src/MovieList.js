7137f88e08cacf5b50f55e88a3c72760d9a6761a
Nirmal2349 <nirmalrajr35@gmail.com>
2022-02-21 17:03:35 +0530

    edit movie

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,9 +1,11 @@
 import { Movie } from "./Movie";
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export function MovieList({ movieList, setMovieList }) {
+  const history = useHistory();
   return (
     <div className="movie-list">
       {movieList.map(({ name, poster, rating, summary }, index) => (
@@ -14,25 +16,32 @@ export function MovieList({ movieList, setMovieList }) {
           rating={rating}
           summary={summary}
           deleteButton={
+            <IconButton
+              onClick={() => {
+                console.log(index);
 
-            <IconButton onClick={() => {
-
-              console.log(index)
-
-              const copyMovieList = [...movieList];
-              copyMovieList.splice(index, 1);
-              setMovieList(copyMovieList);
-
-            }}
+                const copyMovieList = [...movieList];
+                copyMovieList.splice(index, 1);
+                setMovieList(copyMovieList);
+              }}
               aria-label="delete button"
-              color="error">
-
-              <DeleteIcon /> </IconButton>
+              color="error"
+            >
+              <DeleteIcon />
+            </IconButton>
+          }
+          editButton={
+            <IconButton
+              onclick={() => history.push(`/movies/edit/${index}`)}
+              aria-label="edit"
+              color="secondary"
+            >
+              <EditIcon />
+            </IconButton>
           }
           id={index}
         />
       ))}
-
     </div>
   );
 }
